3e1657e66c925b52501fd28638c9f495d955102e
jlhs1001 <40070946+jlhs1001@users.noreply.github.com>
2020-02-17 11:05:44 -0600

    add game loop and player.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-function gameLoop() {
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 
+let player = {"x": 50, "y": 50, "width": 50, "height": 50};
+
+function update(progress) {
+    // Update the state of the world for the elapsed time since last render
 }
+
+function draw() {
+    // Player
+    ctx.fillStyle = "rgb(0, 255, 0)";
+    ctx.fillRect(player.x, player.y, player.width, player.height)
+}
+
+function loop(timestamp) {
+    let progress = timestamp - lastRender;
+
+    update(progress);
+    draw();
+
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop)
+}
+let lastRender = 0;
+window.requestAnimationFrame(loop);
