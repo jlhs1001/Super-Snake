69a0201e1ef0feea12bad320867ac162875d11b6
Heith Seewald <heith.seewald@hp.com>
2020-02-17 19:45:38 -0600

    Add new snake game object
    Replaces "player"

--- js/main.js
+++ js/main.js
@@ -1,27 +1,51 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-
+canvas.width = 800;
+canvas.height = 600;
 let score = 0;
+let dx = 32;
 
 let gameState = true;
 
 let apple = null;
 let player = null;
+let snake = [
+    {x: 32, y: 256},
+    {x: 64, y: 256},
+    {x: 96, y: 256},
+    {x: 128, y: 256},
+    {x: 160, y: 256}
+];
+
+let head = {x: snake[0].x + dx, y: snake[0].y};
+
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = "lightgreen";
+    ctx.strokestyle = "darkgreen";
+
+    ctx.fillRect(snakePart.x, snakePart.y, 32, 32);
+    ctx.strokeRect(snakePart.x, snakePart.y, 32, 32);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+function advanceSnake() {
+    snake.unshift(head);
+    snake.pop();
+}
 
 let lives = 3;
 
 let speedIncrement = 1.15;
 
-let playerSegments = [];
-
 
 function spawnPlayer(parent = null) {
-
-
     player = {
         parent: parent,
-        x: 50,
-        y: 50,
+        x: 0,
+        y: 0,
         width: 32,
         height: 32,
         speed: 1,
@@ -42,27 +66,21 @@ function spawnPlayer(parent = null) {
         player.speed = parent.speed;
         playerSegments.push(parent);
     }
-
-
 }
-
 
 let direction;
 
-canvas.width = 1280;
-canvas.height = 720;
-
 function spawnApple() {
     apple = {
-        "x": Math.floor(Math.random() * canvas.width - 50),
-        "y": Math.floor(Math.random() * canvas.height - 50),
-        "width": 32,
-        "height": 32
+        x: Math.floor(Math.random() * canvas.width - 32),
+        y: Math.floor(Math.random() * canvas.height - 32),
+        width: 32,
+        height: 32
     };
     if (apple.x < apple.width) {
-        apple.x +=  apple.width
+        apple.x += apple.width;
     } else if (apple.y < apple.width) {
-        apple.y += apple.height
+        apple.y += apple.height;
     }
     ctx.fillStyle = "rgb(255,42,46)";
     ctx.fillRect(apple.x, apple.y, apple.width, apple.height);
@@ -90,16 +108,21 @@ document.addEventListener("keydown", function (e) {
             player.speed *= 2;
             break;
         case "KeyK":
-            player.speed *= .5;
+            player.speed *= 0.5;
             break;
     }
-
 });
 
 
 function update(progress) {
-
-    if (player && (player.x > canvas.width || player.x < 0 || player.y > canvas.height || player.y < 0)) {
+    advanceSnake();
+    if (
+        player &&
+        (player.x > canvas.width ||
+            player.x < 0 ||
+            player.y > canvas.height ||
+            player.y < 0)
+    ) {
         spawnPlayer();
         lives--;
 
@@ -107,42 +130,40 @@ function update(progress) {
 
     if (player && player.isCollided()) {
         spawnApple();
-        spawnPlayer(parent = player);
+        spawnPlayer((parent = player));
         player.speed *= speedIncrement;
         score++;
         console.log(score);
     }
     switch (direction) {
         case 1:
-            player.y += -1 * player.speed * 2;
+            head = {x: snake[0].x, y: snake[0].y - dx};
             break;
         case 2:
-            player.y += player.speed * 2;
+            head = {x: snake[0].x, y: snake[0].y + dx};
             break;
         case 3:
-            player.x += -1 * player.speed * 2;
+            head = {x: snake[0].x - dx, y: snake[0].y};
             break;
         case 4:
-            player.x += player.speed * 2;
+            head = {x: snake[0].x + dx, y: snake[0].y};
             break;
     }
-    // console.log(`playerX: ${player.x} playerY: ${player.y}`);
+
 }
 
 function draw() {
-    // Player
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawSnake();
     ctx.fillStyle = "rgb(59,255,119)";
     ctx.fillRect(player.x, player.y, player.width, player.height);
     ctx.fillStyle = "red";
     ctx.fillRect(apple.x, apple.y, apple.width, apple.height);
-
     ctx.fillStyle = "black";
     ctx.font = "30px Arial";
     ctx.fillText(`score: ${score.toString()}`, canvas.width / 2 - 300, 30);
-
     ctx.fillText(`speed: ${player.speed.toFixed(2)}`, canvas.width / 2 - 170, 30);
-
     ctx.fillText(`Life: ${lives}`, canvas.width / 2 + 15, 30);
 
     if (lives <= 0) {
@@ -173,9 +194,8 @@ function loop(timestamp) {
 
     draw();
 
-
     lastRender = timestamp;
-    window.requestAnimationFrame(loop)
+    window.requestAnimationFrame(loop);
 }
 
 let lastRender = 0;
